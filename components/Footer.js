eabfcedc794f61d99800391e2202223ecc10215f
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-07-24 14:28:09 +0530

    Change in footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-gray-900 text-white flex items-center justify-center h-10 px-4'>
-        <p className='text-center'>Copyright &copy; Get me a chai - All rights reserved</p>
+        <p className='text-center'>Copyright &copy; eventure - All rights reserved</p>
     </footer>
   )
 }

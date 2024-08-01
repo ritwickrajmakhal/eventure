4bfd369f7142d0178ba996809c78e736ee7d6b5f
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-08-01 13:49:32 +0530

    nav-items: active/passive loading on mobile ph issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,37 @@
-"use client"
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import React, { useState, useRef, useEffect } from 'react'
-import Link from 'next/link'
-
-const navigation = [
-  { name: 'Events', href: '/events', current: false },
-  { name: 'Services', href: '/services', current: false },
-  { name: 'About', href: '/about', current: false },
-  { name: 'Contact', href: '/contact', current: false },
-]
+"use client";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import React, { useState, useRef, useEffect } from "react";
+import Link from "next/link";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
-  const [showDropdown, setShowDropdown] = useState(false)
-  const dropdownRef = useRef(null)
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const [navigation, setNavigation] = useState([
+    { name: "Home", href: "/", current: true },
+    { name: "Events", href: "/events", current: false },
+    { name: "Services", href: "/services", current: false },
+    { name: "About", href: "/about", current: false },
+    { name: "Contact", href: "/contact", current: false },
+  ]);
 
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setShowDropdown(false)
+        setShowDropdown(false);
       }
     }
 
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [dropdownRef])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -51,19 +52,33 @@ export default function Navbar() {
             </Disclosure.Button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            {/* logo */}
             <div className="flex flex-shrink-0 items-center">
-              <Link className='text-white font-bold text-xl' href='/'>Eventure</Link>
+              <h1 className="text-white font-bold text-xl" href="/">
+                Eventure
+              </h1>
             </div>
+            {/* navigation */}
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
                   <Link
                     key={item.name}
+                    onClick={() =>
+                      setNavigation(
+                        navigation.map((nav) => ({
+                          ...nav,
+                          current: nav.name === item.name,
+                        }))
+                      )
+                    }
                     href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? "page" : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "rounded-md px-3 py-2 text-sm font-medium"
                     )}
                   >
                     {item.name}
@@ -72,6 +87,8 @@ export default function Navbar() {
               </div>
             </div>
           </div>
+
+          {/* login button */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
@@ -82,26 +99,41 @@ export default function Navbar() {
         </div>
       </div>
 
+
       {showDropdown && (
         <div ref={dropdownRef} className="sm:hidden">
           <div className="space-y-1 px-2 pb-3 pt-2">
             {navigation.map((item) => (
               <Disclosure.Button
                 key={item.name}
-                as="a"
-                href={item.href}
-                aria-current={item.current ? 'page' : undefined}
+                as="div"
+                aria-current={item.current ? "page" : undefined}
                 className={classNames(
-                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium',
+                  item.current
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium"
                 )}
               >
-                {item.name}
+                <Link
+                  href={item.href}
+                  className="block h-full w-full"
+                  onClick={() =>
+                    setNavigation(
+                      navigation.map((nav) => ({
+                        ...nav,
+                        current: nav.name === item.name,
+                      }))
+                    )
+                  }
+                >
+                  {item.name}
+                </Link>
               </Disclosure.Button>
             ))}
           </div>
         </div>
       )}
     </Disclosure>
-  )
+  );
 }

70fe72d84086132fad39a2e2779d41b8e39e6201
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-08-23 23:51:29 +0530

    feat: Add login provider component and update login page

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
+
+export async function signup(formData) {
+
+}

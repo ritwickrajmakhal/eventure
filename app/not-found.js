5a6700993d5bfec439f2f542892c824dabd8c3cb
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-08-23 22:58:23 +0530

    Add 404 page component for handling missing pages

---
+++ app/not-found.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="bg-transparent">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            Something&apos;s missing.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the
+            home page.{" "}
+          </p>
+          <Link
+            href="/"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}

e7082fa0b0162d72f236e20995cc809b09c46e82
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-03 23:26:27 +0530

    authentication using credentials implemented

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import request from "@/lib/request";
 
 const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -19,12 +25,58 @@ const handler = NextAuth({
         },
       },
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        try {
+          const response = await request("/api/auth/local", {
+            method: "POST",
+            body: {
+              identifier: credentials.email,
+              password: credentials.password,
+            },
+          });
+
+          if (response.result) {
+            const user = response.result.user;
+            return {
+              id: user.id,
+              name: user.username,
+              email: user.email,
+              provider: user.provider,
+            };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.provider = user.provider;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.provider = token.provider;
+      }
+      return session;
+    },
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
-
-export async function signup(formData) {
-
-}

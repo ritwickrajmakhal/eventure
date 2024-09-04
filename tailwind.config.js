a8a424020dd0f54c52d17efb118cb30a85dfa79f
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-04 19:19:31 +0530

    chore: Update Tailwind config with Flowbite integration and dark mode support, and update npm dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };

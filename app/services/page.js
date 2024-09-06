53bcabfa351c5281dfbe34435cbe2a3eabac03c6
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-06 09:47:50 +0530

    Implement card component to reduce the code

--- app/services/page.js
+++ app/services/page.js
@@ -1,69 +1,77 @@
-import React from 'react'
-import './style.css';
+import "./style.css";
 
 const page = () => {
+  const data = [
+    {
+        id: 1,
+        icon: "fas fa-map-marker-alt",
+        title: "venue selection",
+        desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+      },
+    {
+      id: 2,
+      icon: "fas fa-envelope",
+      title: "invitation card",
+      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+    },
+    {
+      id: 3,
+      icon: "fas fa-music",
+      title: "entertainment",
+      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+    },
+    {
+      id: 4,
+      icon: "fas fa-utensils",
+      title: "food and drinks",
+      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+    },
+    {
+      id: 5,
+      icon: "fas fa-photo-video",
+      title: "photos and videos",
+      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+    },
+    {
+      id: 6,
+      icon: "fas fa-birthday-cake",
+      title: "custom food",
+      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
+    },
+  ];
   return (
-    <div className='text-white'>
-        
-<header className="header">
+    <div className="text-white">
+      {/* Font */}
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      ></link>
 
-    {/* Font */}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"></link>
-</header>
-
-
-
-  {/* service section  */}
-    <section className="service" id="service">
-        <h1 className="heading"> Our <span>Services</span></h1>
-
-            <div className='box-container'>
-                <div className='box'>
-                    <i className='fas fa-map-marker-alt'></i>
-                    <h3>venue selection</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-
-                <div className='box'>
-                    <i className='fas fa-envelope'></i>
-                    <h3>invitation card</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-
-                <div className='box'>
-                    <i className='fas fa-music'></i>
-                    <h3>entertainment</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-
-                <div className='box'>
-                    <i className='fas fa-utensils'></i>
-                    <h3>food and drinks</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-
-                <div className='box'>
-                    <i className='fas fa-photo-video'></i>
-                    <h3>photos and videos</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-
-                <div className='box'>
-                    <i className='fas fa-birthday-cake'></i>
-                    <h3>custom food</h3>
-                    <p>Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.
-                    </p>
-                </div>
-            </div>
-
-    </section>    
+      {/* service section  */}
+      <section className="services" id="services">
+        <h1 className="heading">
+          {" "}
+          Our <span>Services</span>
+        </h1>
+        {/* cards */}
+        <div className="box-container">
+          {data.map((card) => (
+            <Card key={card.id} card={card} />
+          ))}
+        </div>
+      </section>
     </div>
-    )
-}
+  );
+};
 
-export default page
+const Card = ({ card }) => {
+  return (
+    <div className="box">
+      <i className={card.icon}></i>
+      <h3>{card.title}</h3>
+      <p>{card.desc}</p>
+    </div>
+  );
+};
+
+export default page;

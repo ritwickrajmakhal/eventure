cba9251d19e1d42f622443d13bf07d0ceed0ffdc
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-11 16:50:44 +0530

    feat: Update URL configuration in server.js
    Update the URL configuration in server.js to include a proxy setting. This allows the server to work behind a reverse proxy. The URL is now set to 'http://localhost:1337' and the proxy setting is enabled.

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL','http://localhost:1337'),
+  url: env('URL', 'http://localhost:1337'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },

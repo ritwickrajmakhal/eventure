34bfd1671baf33745b66d4a36e804f0a12887331
Rupam Bhakta <109302173+rupambhakta@users.noreply.github.com>
2024-09-19 01:25:36 +0530

    Event templetes page complete

--- app/events/page.js
+++ app/events/page.js
@@ -1,82 +1,148 @@
-import React from 'react'
+"use client";
+import { React, useEffect, useState } from 'react'
 import Link from 'next/link'
-import Card3 from '@/components/Card3'
+import request from '@/lib/request'
+import Image from 'next/image';
 
-const page = () => {
-  const cardData = [
-    {
-      className: "conference",
-      img: "conference",
-      heading: "Conference",
-      paragraph:
-        "Allow yourself to organize a conference succfully",
-    },
-    {
-      className: "tradeShow",
-      img: "tradeshow1",
-      heading: "Trade Show",
-      paragraph:
-        "Allow yourself to organize a conference succfully",
-    },
-    {
-      className: "engagement",
-      img: "engagement",
-      heading: "Engagement",
-      paragraph:
-        "Allow yourself to organize a conference succfully",
-    },
-    {
-      className: "birthday",
-      img: "birthday",
-      heading: "Birthday",
-      paragraph:
-        "Allow yourself to organize a conference succfully",
-    },
-    {
-      className: "cerimonial",
-      img: "cerimonial",
-      heading: "Cerimoni",
-      paragraph:
-        "Allow yourself to organize a conference succfully",
-    },
-    {
-      className: "custom",
-      img: "custom",
-      heading: "Create Custom",
-      paragraph:
-        "Create your own event and colobarate with us and organize a grate event with us...",
-    },
-  ]
+const Page = () => {
+  // const cardData = [
+  //   {
+  //     className: "conference",
+  //     img: "conference",
+  //     heading: "Conference",
+  //     paragraph:
+  //       "Allow yourself to organize a conference succfully",
+  //   },
+  //   {
+  //     className: "tradeShow",
+  //     img: "tradeshow1",
+  //     heading: "Trade Show",
+  //     paragraph:
+  //       "Allow yourself to organize a conference succfully",
+  //   },
+  //   {
+  //     className: "engagement",
+  //     img: "engagement",
+  //     heading: "Engagement",
+  //     paragraph:
+  //       "Allow yourself to organize a conference succfully",
+  //   },
+  //   {
+  //     className: "birthday",
+  //     img: "birthday",
+  //     heading: "Birthday",
+  //     paragraph:
+  //       "Allow yourself to organize a conference succfully",
+  //   },
+  //   {
+  //     className: "cerimonial",
+  //     img: "cerimonial",
+  //     heading: "Cerimoni",
+  //     paragraph:
+  //       "Allow yourself to organize a conference succfully",
+  //   },
+  //   {
+  //     className: "custom",
+  //     img: "custom",
+  //     heading: "Create Custom",
+  //     paragraph:
+  //       "Create your own event and colobarate with us and organize a grate event with us...",
+  //   },
+  // ]
+  const [cardData, setCardData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await request("/api/event-templates?populate=*&sort=id");
+
+        if (response.error) {
+          console.log(response.error);
+
+        } else {
+          let newCardData = [...cardData];
+          for (const i of response.data) {
+            newCardData.push({
+              id: i.id,
+              img: `${process.env.NEXT_PUBLIC_API_URL}${i.attributes.image.data[0].attributes.url}`,
+              heading: i.attributes.title,
+              paragraph: i.attributes.description,
+              slug: i.attributes.slug
+            })
+          }
+          setCardData(newCardData);
+
+        }
+      } catch (error) {
+        console.log(error);
+
+      }
+    }
+    fetchData();
+  }, [cardData]);
+
+
+
   return (
     <div className='text-white flex flex-col justify-center items-center'>
 
       {/* 1st row */}
       <div className="fearures flex mt-5 md:flex-row flex-col items-center gap-5 md:gap-5">
         {cardData.slice(0, 3).map((data, index) => (
-          <Card3
+          <Card
             key={index}
-            className={data.className}
-            img={data.img + ".jpg"}
+            className={data.slug}
+            img={data.img}
             heading={data.heading}
             paragraph={data.paragraph}
           />
         ))}
       </div>
       <div className="fearures flex m-5 md:flex-row flex-col items-center gap-5 md:gap-5">
-        {cardData.slice(3, 6).map((data, index) => (
-          <Card3
+        {cardData.slice(3, 5).map((data, index) => (
+          <Card
             key={index}
-            className={data.className}
-            img={data.img + ".jpg"}
+            className={data.slug}
+            img={data.img}
             heading={data.heading}
             paragraph={data.paragraph}
           />
         ))}
+        <Card
+          className={"custom"}
+          img={"/custom.jpg"}
+          heading={"Create Custom "}
+          paragraph={"Create your own event and collaborate with us and organize a grate event. Click Continue to procide..."}
+        />
       </div>
+
 
 
     </div >
   )
 }
 
-export default page
+function Card({ className, img, heading, paragraph }) {
+  return (
+    <div className={className}>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <Link href={`./event/${className}`}>
+          <Image className="rounded-t-lg" src={img} alt="image" width={500} height={250}/>
+        </Link>
+        <div className="p-5">
+          <Link href={`./event/${className}`}>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{heading}</h5>
+          </Link>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{paragraph}</p>
+          <Link href={`./event/${className}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Contunue
+            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Page
--- components/CustomerReview.js
+++ components/CustomerReview.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 const CustomerReview = () => {
     return (
         <div>
-            <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
+            {/* <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
                 <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                     <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
                         <p class="my-4">If you care for your time, I hands down would go with this.</p>
                     </blockquote>
                     <figcaption class="flex items-center justify-center ">
-                        <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" />
+                        <Image class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" height={0} width={0} />
                         <div class="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
                             <div>Bonnie Green</div>
                             <div class="text-sm text-gray-500 dark:text-gray-400 ">Developer at Open AI</div>
@@ -23,7 +24,7 @@ const CustomerReview = () => {
                         <p class="my-4">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!</p>
                     </blockquote>
                     <figcaption class="flex items-center justify-center ">
-                        <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture" />
+                        <Image class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture" height={0} width={0} />
                         <div class="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
                             <div>Roberta Casas</div>
                             <div class="text-sm text-gray-500 dark:text-gray-400">Lead designer at Dropbox</div>
@@ -36,7 +37,7 @@ const CustomerReview = () => {
                         <p class="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application.</p>
                     </blockquote>
                     <figcaption class="flex items-center justify-center ">
-                        <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture" />
+                        <Image class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture" height={0} width={0} />
                         <div class="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
                             <div>Jese Leos</div>
                             <div class="text-sm text-gray-500 dark:text-gray-400">Software Engineer at Facebook</div>
@@ -49,14 +50,14 @@ const CustomerReview = () => {
                         <p class="my-4">You have many examples that can be used to create a fast prototype for your team.</p>
                     </blockquote>
                     <figcaption class="flex items-center justify-center ">
-                        <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture" />
+                        <Image class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture" height={0} width={0} />
                         <div class="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
                             <div>Joseph McFall</div>
                             <div class="text-sm text-gray-500 dark:text-gray-400">CTO at Google</div>
                         </div>
                     </figcaption>
                 </figure>
-            </div>
+            </div> */}
 
         </div>
     )

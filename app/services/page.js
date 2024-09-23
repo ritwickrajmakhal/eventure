2788fd4c306c90cd647e46c95cf8ace71b72a079
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-23 07:40:44 +0530

    Refactor services page to fetch data from API and update rendering logic

--- app/services/page.js
+++ app/services/page.js
@@ -1,44 +1,10 @@
+import request from "@/lib/request";
 import "./style.css";
 
-const page = () => {
-  const data = [
-    {
-        id: 1,
-        icon: "fas fa-map-marker-alt",
-        title: "venue selection",
-        desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-      },
-    {
-      id: 2,
-      icon: "fas fa-envelope",
-      title: "invitation card",
-      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-    },
-    {
-      id: 3,
-      icon: "fas fa-music",
-      title: "entertainment",
-      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-    },
-    {
-      id: 4,
-      icon: "fas fa-utensils",
-      title: "food and drinks",
-      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-    },
-    {
-      id: 5,
-      icon: "fas fa-photo-video",
-      title: "photos and videos",
-      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-    },
-    {
-      id: 6,
-      icon: "fas fa-birthday-cake",
-      title: "custom food",
-      desc: "Users access websites using web browsers and can navigate through the pages by clicking on links or using menus. A web service is a technological component of the internet that facilitates communication and data exchange between different software applications.",
-    },
-  ];
+const page = async () => {
+  const res = await request("/api/services-page?populate=services");
+  const { services } = res.data.attributes;
+
   return (
     <div className="text-white">
       {/* Font */}
@@ -50,26 +16,19 @@ const page = () => {
       {/* service section  */}
       <section className="services" id="services">
         <h1 className="heading">
-          {" "}
           Our <span>Services</span>
         </h1>
         {/* cards */}
         <div className="box-container">
-          {data.map((card) => (
-            <Card key={card.id} card={card} />
+          {services.map((service, index) => (
+            <div className="box" key={index}>
+              <i className={service.icon}></i>
+              <h3>{service.heading}</h3>
+              <p>{service.description}</p>
+            </div>
           ))}
         </div>
       </section>
-    </div>
-  );
-};
-
-const Card = ({ card }) => {
-  return (
-    <div className="box">
-      <i className={card.icon}></i>
-      <h3>{card.title}</h3>
-      <p>{card.desc}</p>
     </div>
   );
 };

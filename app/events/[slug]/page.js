854e79a32b3bb17f82f11dfe02aa06df77c7e093
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-09-24 22:13:52 +0530

    Refactor event page and Plan component for improved service handling; remove unused code and update links

--- app/events/[slug]/page.js
+++ app/events/[slug]/page.js
@@ -8,14 +8,14 @@ import request from "@/lib/request";
 
 export default async function Page({ params }) {
   const res = await request(
-    `/api/event-templates?populate=plans.services&filters[slug][$eq]=${params.slug}&populate=our_helps.image&populate=image&populate=customer_reviews.profilePic&populate=profilePic`
+    `/api/event-templates?populate=plans.services&filters[slug][$eq]=${params.slug}&populate=our_helps.image&populate=image&populate=customer_reviews.profilePic`
   );
   const eventData = res.data[0];
 
   return (
     <div className="text-white w-4/5 m-auto">
       <div className="gallery py-5 flex flex-wrap gap-5 justify-center">
-        {eventData?.attributes.image.data.slice(1,7).map((img, index) => (
+        {eventData?.attributes.image.data.slice(1, 7).map((img, index) => (
           <Gallery
             key={index}
             img={(process.env.NEXT_PUBLIC_API_URL || "") + img.attributes.url}
@@ -84,15 +84,15 @@ export default async function Page({ params }) {
           </h1>
         </div>
 
-        <div className="flex justify-between w-5/6 m-auto">
+        <div className="flex justify-between w-5/6 m-auto overflow-auto">
           {eventData?.attributes.plans.map((plan, index) => (
             <Plan
               key={index}
               price={plan.price}
-              allServices={
-                eventData?.attributes.plans[
-                  eventData?.attributes.plans.length - 1
-                ].services.data
+              premiumServices={
+                eventData?.attributes.plans.filter(
+                  (plan) => plan.type === "Premium"
+                )[0].services.data
               }
               services={plan.services.data}
               type={plan.type}
@@ -100,43 +100,6 @@ export default async function Page({ params }) {
           ))}
         </div>
       </div>
-
-      <div className="createCustom flex justify-center align-middle items-center m-12">
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Create your own plan as per your requerment.
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <Link
-            href="/events/create"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Create Now
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </Link>
-        </div>
-      </div>
-
-      <div className="calendar">{/* TODO */}</div>
     </div>
   );
 }
--- components/Plan.js
+++ components/Plan.js
@@ -1,15 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
-const Plan = ({ type, services, allServices, price }) => {
-  let serviceTitles = [];
-  for (const service of services) {
-    serviceTitles.push(service.attributes.title);
-  }
-  let allServiceTitles = [];
-  for (const service of allServices) {
-    allServiceTitles.push(service.attributes.title);
-  }
+const Plan = ({ type, services, premiumServices, price }) => {
+  const unavailableServices = premiumServices.filter(
+    (premiumService) =>
+      !services.some((service) => service.id === premiumService.id)
+  );
   return (
     <div>
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -26,39 +22,38 @@ const Plan = ({ type, services, allServices, price }) => {
           </span>
         </div>
         <ul role="list" className="space-y-5 my-7">
-          {allServiceTitles.map((title, index) => (
+          {services.map((service, index) => (
+            <li className="flex items-center" key={index}>
+              <svg
+                className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
+              <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                {service.attributes.title}
+              </span>
+            </li>
+          ))}
+          {unavailableServices.map((service, index) => (
             <li
-              className={`flex items-center ${
-                serviceTitles.includes(title)
-                  ? ""
-                  : "line-through decoration-gray-500"
-              }`}
+              className="flex items-center line-through decoration-gray-500"
               key={index}
             >
-              {serviceTitles.includes(title) ? (
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-              ) : (
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
-              )}
-
+              <svg
+                className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
               <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                {title}
+                {service.attributes.title}
               </span>
             </li>
           ))}
@@ -67,7 +62,7 @@ const Plan = ({ type, services, allServices, price }) => {
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
         >
-          <Link href={"/createevent"}>Choose plan</Link>
+          <Link href={"/events/create"}>Choose plan</Link>
         </button>
       </div>
     </div>

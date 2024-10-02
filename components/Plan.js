0de723dd32b8ba3c41a34b07c98cdb1efaab806c
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-10-02 16:11:14 +0530

    Refactor Plan component to include total cost and update "Choose plan" button based on plan selection

--- components/Plan.js
+++ components/Plan.js
@@ -1,67 +1,44 @@
 import Link from "next/link";
 import React from "react";
+import { HiBadgeCheck } from "react-icons/hi";
 
-const Plan = ({ slug, type, services, premiumServices, price }) => {
+const Plan = ({ slug, type, services, premiumServices, planSelected }) => {
   const unavailableServices = premiumServices.filter(
-    (premiumService) =>
-      !services.some((service) => service.id === premiumService.id)
+    (premium) => !services.some((service) => service.id === premium.id)
   );
+  const totalCost = services.reduce((acc, { attributes }) => acc + attributes.cost, 0);
+
   return (
-    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-        {type}
-      </h5>
+    <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{type}</h5>
       <div className="flex items-baseline text-gray-900 dark:text-white">
-        <span className="text-3xl font-semibold">$</span>
-        <span className="text-5xl font-extrabold tracking-tight">
-          {price}
-        </span>
-        <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-          /month
-        </span>
+        <span className="text-3xl font-semibold">₹</span>
+        <span className="text-5xl font-extrabold">{totalCost}</span>
+        <span className="ms-1 text-xl text-gray-500 dark:text-gray-400">/hrs</span>
       </div>
-      <ul role="list" className="space-y-5 my-7">
-        {services.map((service, index) => (
+      <ul className="space-y-5 my-7">
+        {services.map(({ attributes }, index) => (
           <li className="flex items-center" key={index}>
-            <svg
-              className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-              {service.attributes.title}
-            </span>
+            <HiBadgeCheck className="w-5 h-5 text-blue-700 dark:text-blue-500" />
+            <span className="ms-3 text-base text-gray-500 dark:text-gray-400">{attributes.title}</span>
           </li>
         ))}
-        {unavailableServices.map((service, index) => (
-          <li
-            className="flex items-center line-through decoration-gray-500"
-            key={index}
-          >
-            <svg
-              className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-              {service.attributes.title}
-            </span>
+        {unavailableServices.map(({ attributes }, index) => (
+          <li className="flex items-center line-through" key={index}>
+            <HiBadgeCheck className="w-5 h-5 text-gray-700 dark:text-gray-500" />
+            <span className="ms-3 text-base text-gray-500 dark:text-gray-400">{attributes.title}</span>
           </li>
         ))}
       </ul>
-      <Link href={`/events/create?template=${slug}&plan=${type}`}>
+      <Link href={`/events/create?template=${slug}${planSelected ? "" : `&plan=${type}`}`}>
         <button
           type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-        >Choose plan
+          className={`text-white ${planSelected ? "bg-red-700 hover:bg-red-800" : "bg-blue-700 hover:bg-blue-800"} 
+            focus:ring-4 focus:outline-none focus:ring-${planSelected ? "red" : "blue"}-200 
+            dark:bg-${planSelected ? "red" : "blue"}-600 dark:hover:bg-${planSelected ? "red" : "blue"}-700 
+            dark:focus:ring-${planSelected ? "red" : "blue"}-900 font-medium rounded-lg text-sm px-5 py-2.5 w-full`}
+        >
+          {planSelected ? "Remove Plan" : "Choose plan"}
         </button>
       </Link>
     </div>

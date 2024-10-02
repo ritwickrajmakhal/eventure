bf5fd3ae132a79accd69fc2528a18d5443119049
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-10-02 21:30:04 +0530

    Refactor EventTable component to display event creation date instead of cost and participants

--- app/dashboard/events/EventTable.js
+++ app/dashboard/events/EventTable.js
@@ -6,21 +6,19 @@ const EventTable = ({ events }) => (
       <Table.Head>
         <Table.HeadCell>Event name</Table.HeadCell>
         <Table.HeadCell>Status</Table.HeadCell>
-        <Table.HeadCell>Cost</Table.HeadCell>
-        <Table.HeadCell>Participants</Table.HeadCell>
+        <Table.HeadCell>createdAt</Table.HeadCell>
         <Table.HeadCell>
           <span className="sr-only">Edit</span>
         </Table.HeadCell>
       </Table.Head>
       <Table.Body className="divide-y">
-        {events?.map(({ id, attributes: { name, status, total_cost = 0, audience } }) => (
+        {events?.map(({ id, attributes: { name, status, createdAt } }) => (
           <Table.Row key={id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               {name}
             </Table.Cell>
             <Table.Cell>{status}</Table.Cell>
-            <Table.Cell>{total_cost || 0}</Table.Cell>
-            <Table.Cell>{audience?.data?.details?.length || 0}</Table.Cell>
+            <Table.Cell>{new Date(createdAt).toLocaleDateString('en-GB')}</Table.Cell>
             <Table.Cell>
               <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
                 Edit

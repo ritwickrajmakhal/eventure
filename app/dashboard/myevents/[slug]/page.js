be67363b899101c563631c05ef5a44cc6dbb6bf6
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-10-09 19:36:03 +0530

    Refactor EventDetails component to display event details, venue, schedule, services, and audience information

--- app/dashboard/myevents/[slug]/page.js
+++ app/dashboard/myevents/[slug]/page.js
@@ -1,65 +1,29 @@
 "use client";
-import { HiCalendar } from "react-icons/hi";
-import { Timeline } from "flowbite-react";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import request from "@/lib/request";
+import EventDetails from "./EventDetails";
 
-const page = () => {
+const EventPage = ({ params }) => {
+    const [session, setSession] = useState(Cookies.get("session") ? JSON.parse(Cookies.get("session")) : null);
+    const [event, setEvent] = useState(null);
+
+    useEffect(() => {
+        if (session) {
+            (async () => {
+                const event = await request(
+                    `/api/events?filters[user][$eq]=${session.id}&filters[slug][$eq]=${params.slug}&populate=venue,schedules,services,audiences`,
+                    { headers: { Authorization: `Bearer ${session.jwt}` } }
+                );
+                setEvent(event.data[0]);
+            })();
+        }
+    }, [session, params]);
     return (
-        <div>
-            <div className="mb-4 dark:bg-gray-800 bg-white rounded-lg lg:p-4 p-2">
-                <h2 className="text-2xl font-bold mb-2">
-                    Event Name
-                </h2>
-                <p className="mb-2"><strong>Description:</strong>
-                    Event Description
-                </p>
-                <p className="mb-2"><strong>Participants:</strong> 200</p>
-            </div>
-            <div className="flex justify-center">
-                <Timeline>
-                    <Timeline.Item>
-                        <Timeline.Point icon={HiCalendar} />
-                        <Timeline.Content>
-                            <Timeline.Time>February 2022</Timeline.Time>
-                            <Timeline.Title>Event created</Timeline.Title>
-                            <Timeline.Body>
-                                The event was created and the initial details were set up.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-                    <Timeline.Item>
-                        <Timeline.Point icon={HiCalendar} />
-                        <Timeline.Content>
-                            <Timeline.Time>March 2022</Timeline.Time>
-                            <Timeline.Title>Send Invitation</Timeline.Title>
-                            <Timeline.Body>
-                                Invitations were sent to the participants.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-                    <Timeline.Item>
-                        <Timeline.Point icon={HiCalendar} />
-                        <Timeline.Content>
-                            <Timeline.Time>April 2022</Timeline.Time>
-                            <Timeline.Title>Event organized</Timeline.Title>
-                            <Timeline.Body>
-                                The event was organized successfully.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-                    <Timeline.Item>
-                        <Timeline.Point icon={HiCalendar} />
-                        <Timeline.Content>
-                            <Timeline.Time>May 2022</Timeline.Time>
-                            <Timeline.Title>Event completed</Timeline.Title>
-                            <Timeline.Body>
-                                The event was completed successfully.
-                            </Timeline.Body>
-                        </Timeline.Content>
-                    </Timeline.Item>
-                </Timeline>
-            </div>
-        </div>
+        <>
+            {event && <EventDetails eventData={event} />}
+        </>
     )
 }
 
-export default page
+export default EventPage
